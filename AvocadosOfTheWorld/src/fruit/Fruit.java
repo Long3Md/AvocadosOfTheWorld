253e98425bfe830bbb40ccfa5260ac18f829ae7f
mark <mark@courtneys-air.uccc.uc.edu>
2020-02-11 12:18:26 -0500

    Added a constructor and some code in the main

--- AvocadosOfTheWorld/src/fruit/Fruit.java
+++ AvocadosOfTheWorld/src/fruit/Fruit.java
@@ -13,5 +13,39 @@ package fruit;
 
 
 public abstract class Fruit {
-
+		
+		private int weight; //in ounces
+		private float cost;
+		
+		
+		public int getWeight() {
+			return weight;
+		}
+		public void setWeight(int weight) {
+			this.weight = weight;
+		}
+		
+		
+		public float getCost() {
+			return cost;
+		}
+		public void setCost(float cost) {
+			this.cost = cost;
+		}
+		public String toString() {
+			return "weight = " + weight + "cost = " + cost;
+		}
+		//Constructor because same name as class
+		//Could use the this. method as well or the setProperty method
+		/***
+		 * Constructor
+		 * @param weight of Fruit
+		 * @param cost of Fruit
+		 */
+		public Fruit(int weight, float cost){
+			setWeight(weight);
+			setCost(cost);
+			
+		}
+		
 }
--- AvocadosOfTheWorld/src/main/Main.java
+++ AvocadosOfTheWorld/src/main/Main.java
@@ -4,12 +4,18 @@
  * 
  */
 package main;
- 
+
+import fruit.Avocado;
+import fruit.Fruit;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+		//declare and instantiate a Fruit called avocajoe
+		//Fruit avocajoe = new Fruit(); //Cannot instantiate an abstract class
+										// be able to explain why we have abstract classes 
+		//declare and instanitate an Avocado name acoajoe
+		Avocado avacajoe = new Avocado(6,(float).50);
 
 	}
 }
